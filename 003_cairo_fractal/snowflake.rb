5d8c871c4f8a58f996c13567e0c3b06244193f74
鈴木竜太 <django.contrib.auth.models@gmail.com>
2021-09-18 21:01:56 +0900

    prepare kogh snowflake: draw triangle

---
+++ 003_cairo_fractal/snowflake.rb
@@ -0,0 +1,31 @@
+require 'cairo'
+
+size = 600.0
+margin = 100.0
+
+surface = Cairo::ImageSurface.new(:argb32, size, size)
+context = Cairo::Context.new(surface)
+
+# 背景
+context.fill do
+  context.set_source_color(Cairo::Color::WHITE)
+  context.rectangle(0, 0, size, size)
+end
+
+context.stroke do
+  # 第1段階
+  context.set_source_color(Cairo::Color::BLACK)
+  top_x = size / 2
+  top_y = margin
+  context.move_to(top_x, top_y)
+  triangle_length = size - (margin * 2)
+  triangle_x1 = top_x - (triangle_length / 2)
+  triangle_y = margin + (triangle_length * (Math.sqrt(3) / 2))
+  context.line_to(triangle_x1, triangle_y)
+  triangle_x2 = top_x + (triangle_length / 2)
+  context.line_to(triangle_x2, triangle_y)
+  context.line_to(top_x, top_y)
+end
+
+Dir.mkdir('tmp/snowflakes') unless Dir.exist?('tmp/snowflakes')
+context.target.write_to_png('tmp/snowflakes/snowflake.png')
