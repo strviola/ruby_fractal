5ca44bda3aa833f36709d9c736f53809713d810c
鈴木竜太 <django.contrib.auth.models@gmail.com>
2021-09-18 21:25:08 +0900

    use polar coodinates

--- 003_cairo_fractal/snowflake.rb
+++ 003_cairo_fractal/snowflake.rb
@@ -19,10 +19,11 @@ context.stroke do
   top_y = margin
   context.move_to(top_x, top_y)
   triangle_length = size - (margin * 2)
-  triangle_x1 = top_x - (triangle_length / 2)
-  triangle_y = margin + (triangle_length * (Math.sqrt(3) / 2))
+  rad = Math::PI * Rational('2/3')
+  triangle_x1 = top_x - triangle_length * Math.cos(rad)
+  triangle_y = margin + triangle_length * Math.sin(rad)
   context.line_to(triangle_x1, triangle_y)
-  triangle_x2 = top_x + (triangle_length / 2)
+  triangle_x2 = top_x + triangle_length * Math.cos(rad)
   context.line_to(triangle_x2, triangle_y)
   context.line_to(top_x, top_y)
 end
