6a3d29f3c993cdf79561cb3eacee7eee80ee57b5
鈴木竜太 <django.contrib.auth.models@gmail.com>
2021-09-18 18:18:21 +0900

    draw carpet bigger size

--- 003_cairo_fractal/carpet.rb
+++ 003_cairo_fractal/carpet.rb
@@ -1,9 +1,8 @@
 require 'cairo'
 
-format = Cairo::FORMAT_ARGB32
-size = 600.0
+size = 1200.0
 
-surface = Cairo::ImageSurface.new(format, size, size)
+surface = Cairo::ImageSurface.new(Cairo::FORMAT_ARGB32, size, size)
 context = Cairo::Context.new(surface)
 
 # 背景
@@ -12,9 +11,10 @@ context.rectangle(0, 0, size, size)
 context.fill
 context.set_source_rgb(1, 1, 1)
 
-def draw_squares(cairo_context, base_size, step)
+def draw_squares(cairo_surface, cairo_context, base_size, step)
   division_level = 3 ** step
   square_size = base_size / division_level
+  return if square_size < 0.1
   points = (0..division_level).select { _1 % 3 == 1 }
   points.each do |index_x|
     points.each do |index_y|
@@ -24,11 +24,12 @@ def draw_squares(cairo_context, base_size, step)
       cairo_context.fill
     end
   end
+  Dir.mkdir('tmp/carpets') unless Dir.exist?('tmp/carpets')
+  file_name_index = format('%04d', step)
+  cairo_surface.write_to_png("tmp/carpets/carpet_level_#{file_name_index}.png")
 end
 
 # カーペット描画
-draw_squares(context, size, 1)
-draw_squares(context, size, 2)
-draw_squares(context, size, 3)
-
-surface.write_to_png('tmp/sierpinski_carpet.png')
+(1..10).each do |step|
+  draw_squares(surface, context, size, step)
+end
