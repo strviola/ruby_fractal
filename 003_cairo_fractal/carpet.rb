b12bf441f2b9f06a58e1fb5e4df0e32f59547133
鈴木竜太 <django.contrib.auth.models@gmail.com>
2021-09-18 17:47:34 +0900

    draw 2nd step squares

--- 003_cairo_fractal/carpet.rb
+++ 003_cairo_fractal/carpet.rb
@@ -1,17 +1,33 @@
 require 'cairo'
 
 format = Cairo::FORMAT_ARGB32
-width = 600
-height = 600
+size = 600.0
 
-surface = Cairo::ImageSurface.new(format, width, height)
+surface = Cairo::ImageSurface.new(format, size, size)
 context = Cairo::Context.new(surface)
 
-color_white = [1, 1, 1]
-color_purple = [0.5, 0, 0.5]
-
-context.set_source_rgb(*color_white)
-context.rectangle(0, 0, width, height)
+# 背景
+color_purple = [0.2, 0, 0.2]
+context.set_source_rgb(*color_purple)
+context.rectangle(0, 0, size, size)
 context.fill
+
+# 1段階目
+color_white = [1, 1, 1]
+context.set_source_rgb(*color_white)
+next_size = size / 3
+context.rectangle(next_size, next_size, next_size, next_size)
+context.fill
+
+# 2段階目
+next_size = next_size / 3
+[1, 4, 7].each do |index_x|
+  [1, 4, 7].each do |index_y|
+    x = next_size * index_x
+    y = next_size * index_y
+    context.rectangle(x, y, next_size, next_size)
+    context.fill
+  end
+end
 
 surface.write_to_png('tmp/sierpinski_carpet.png')
