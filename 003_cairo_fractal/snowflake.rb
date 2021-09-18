f985cd301d1f734497bf5caa6999101ad75402d0
鈴木竜太 <django.contrib.auth.models@gmail.com>
2021-09-19 00:32:12 +0900

    define class for koch curve

--- 003_cairo_fractal/snowflake.rb
+++ 003_cairo_fractal/snowflake.rb
@@ -1,5 +1,40 @@
 require 'cairo'
 
+class Line
+  attr_accessor :x, :y, :length, :angle
+
+  def initialize(x, y, length, angle)
+    @x = x
+    @y = y
+    @length = length
+    @angle = angle
+  end
+
+  def polar_next_point(base_x, base_y, radius, angle)
+    diff_x = radius * Math.cos(angle)
+    diff_y = radius * Math.sin(angle)
+    [base_x + diff_x, base_y + diff_y]
+  end
+
+  def end_point
+    polar_next_point(@x, @y, @length, @angle)
+  end
+
+  def add_line_koch_step
+    next_length = @length / 3
+    lines = [Line.new(@x, @y, next_length, @angle)]
+    x, y = polar_next_point(@x, @y, next_length, @angle)
+    angle1 = @angle + rad_pi('1/3')
+    lines << Line.new(x, y, next_length, angle1)
+    x, y = polar_next_point(x, y, next_length, angle1)
+    angle2 = @angle + rad_pi('-1/3')
+    lines << Line.new(x, y, next_length, angle2)
+    x, y = polar_next_point(x, y, next_length, angle2)
+    lines << Line.new(x, y, next_length, @angle)
+    lines
+  end
+end
+
 size = 600.0
 margin = 50.0
 base_length = size - (margin * 2)
@@ -19,47 +54,21 @@ def rad_pi(string)
   Math::PI * Rational(string)
 end
 
-def polar_next_point(base_x, base_y, radius, angle)
-  diff_x = radius * Math.cos(angle)
-  diff_y = radius * Math.sin(angle)
-  [base_x + diff_x, base_y + diff_y]
-end
-
-def koch_points_level2(base_x, base_y, base_length)
-  points = [[base_x, base_y]]
-  length = base_length / 3
-  point_x, point_y = polar_next_point(base_x, base_y, length, rad_pi('2/3'))
-  points << [point_x, point_y]
-  point_x, point_y = polar_next_point(point_x, point_y, length, rad_pi(-1))
-  points << [point_x, point_y]
-  point_x, point_y = polar_next_point(point_x, point_y, length, rad_pi('1/3'))
-  points << [point_x, point_y]
-  point_x, point_y = polar_next_point(point_x, point_y, length, rad_pi('2/3'))
-  points << [point_x, point_y]
-  point_x, point_y = polar_next_point(point_x, point_y, length, rad_pi(0))
-  points << [point_x, point_y]
-  point_x, point_y = polar_next_point(point_x, point_y, length, rad_pi('1/3'))
-  points << [point_x, point_y]
-  point_x, point_y = polar_next_point(point_x, point_y, length, rad_pi('-1/3'))
-  points << [point_x, point_y]
-  point_x, point_y = polar_next_point(point_x, point_y, length, rad_pi(0))
-  points << [point_x, point_y]
-  point_x, point_y = polar_next_point(point_x, point_y, length, rad_pi('-2/3'))
-  points << [point_x, point_y]
-  point_x, point_y = polar_next_point(point_x, point_y, length, rad_pi('-1/3'))
-  points << [point_x, point_y]
-  point_x, point_y = polar_next_point(point_x, point_y, length, rad_pi(-1))
-  points << [point_x, point_y]
-  points
-end
-
-context.set_source_color(Cairo::Color::GRAY)
-context.fill do
-  context.move_to(top_x, top_y)
-  koch_points_level2(top_x, top_y, base_length).each do |point_x, point_y|
-    context.line_to(point_x, point_y)
+context.set_source_color(Cairo::Color::BLACK)
+line1 = Line.new(top_x, top_y, base_length, rad_pi('2/3'))
+line2 = Line.new(*line1.end_point, base_length, rad_pi(0))
+line3 = Line.new(*line2.end_point, base_length, rad_pi('-2/3'))
+context.stroke do
+  context.move_to(line1.x, line1.y)
+  line1.add_line_koch_step.each do |line|
+    context.line_to(*line.end_point)
   end
-  context.line_to(top_x, top_y)
+  line2.add_line_koch_step.each do |line|
+    context.line_to(*line.end_point)
+  end
+  line3.add_line_koch_step.each do |line|
+    context.line_to(*line.end_point)
+  end
 end
 
 Dir.mkdir('tmp/snowflakes') unless Dir.exist?('tmp/snowflakes')
