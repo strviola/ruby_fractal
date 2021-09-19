c591b9dfbedd9a466f57825d64013871be87bf28
鈴木竜太 <django.contrib.auth.models@gmail.com>
2021-09-19 17:26:23 +0900

    WIP: define complexes

--- 004_narray/mandel.rb
+++ 004_narray/mandel.rb
@@ -2,6 +2,10 @@ require 'numo/narray'
 require 'gdk_pixbuf2'
 include Numo
 
-# TODO: implement
+def mandel(width, height, width2, height2, zoom)
+  z = (SComplex.new(1, width).seq / width - width2) * zoom +
+      (SComplex.new(height, 1).seq / height - height2) * zoom * Complex(0, 1)
+end
 
-puts true
+data = mandel(600, 600, 0.8, 0.5, 2)
+pp data
